68b29b7d6c6d3740afb178cd2a7f84eef0d9e213
Alejandro <ale.mayen.100@gmail.com>
2022-05-16 22:02:16 -0600

    GitExpertApp

---
+++ src/index.js
@@ -0,0 +1,33 @@
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { GifExpertApp } from './GifExpertApp';
+import './index.css';
+
+
+
+
+
+
+ReactDOM.render( 
+  <GifExpertApp/>, 
+  document.getElementById('root')
+);
+
+
+
+
+
+/*const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <GifExpertApp />
+  </React.StrictMode>
+);
+*/
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//reportWebVitals();
